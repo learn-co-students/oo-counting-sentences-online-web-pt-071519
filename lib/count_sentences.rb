dad3305ec068e302f51dc55a3941548ff8085e25
Paul Beckwith <303dog@gmail.com>
2019-08-14 18:42:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,51 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")      
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+   def count_sentences
+    self.split(/[.?!]+/).grep(/\S/).count
   end
+  
 end
+
+
+
+
+
+
+
+#class String
+
+ # def sentence?
+  #  self.end_with?(".")
+  #end
+ 
+ # def question?
+  #  if self.end_with?("?")
+
+      
+  #end
+
+  #def exclamation?
+   # if self.end_with? == ("!")
+    #  puts "true"
+  #end
+
+  #def count_sentences
+   # self.ends_with? == String.split
+    #  self.length
+  #end
+  #end
+  #end
+  
+#end
